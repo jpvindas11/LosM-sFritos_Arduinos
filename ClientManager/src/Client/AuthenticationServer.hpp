12965ff7400f57933b8e660eff4ab1e761dfefcb
Luis Valverde <lushelosoza77@gmail.com>
2025-09-26 14:30:09 -0600

    Clase abstracta para los servidores e inicio de servidor de autenticación

---
+++ ClientManager/src/Client/AuthenticationServer.hpp
@@ -0,0 +1,12 @@
+#ifndef AUTHENTICATION_SERVER
+#define AUTHENTICATION_SERVER
+
+#include "Server.hpp"
+#include "common.hpp"
+
+class AuthenticationServer: public Server {
+  DISABLE_COPY(AuthenticationServer);
+
+};
+
+#endif // AUTHENTICATION_SERVER
---
+++ ClientManager/src/Client/Server.hpp
@@ -0,0 +1,45 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include "common.hpp"
+#include "Thread.hpp"
+#include "Semaphore.hpp"
+#include <string>
+
+class Server : public virtual Thread {
+  DISABLE_COPY(Server);
+  
+  protected:
+   std::string message;
+   Semaphore canCheckMessage;
+
+  public:
+   explicit Server()
+    : canCheckMessage(0){
+   }
+
+   virtual ~Server(){}
+
+   virtual void waitForMessage(){
+    while(true){
+      this->canCheckMessage.wait();
+      if (message == "closeServer"){
+        break;
+      }
+      this->processMessage();
+    }
+   }
+
+   void setMessage(std::string& message) {
+    this->message = message;
+   }
+
+   void checkMessage(){
+    this->canCheckMessage.signal();
+   }
+
+   virtual void processMessage() = 0;
+   virtual void sendMessage() = 0;
+};
+
+#endif // SERVER_HPP
