470899f4f814d8b932c540bca758b8c62b48e5f8
rair.gomez <rair.gomez@ucr.ac.cr>
2025-10-06 02:29:55 -0600

    código extraer el numero de pagina y offset

--- sistemasOperativos/Tarea2_Virtual_Memory_Manager/src/main.cpp
+++ sistemasOperativos/Tarea2_Virtual_Memory_Manager/src/main.cpp
@@ -1,9 +1,29 @@
 /// @copyright Los Más Fritos - 2025
 
-#include "MemoryManager.hpp"
-#include "TLB.hpp"
+#include <iostream>
+#include <vector>
+#include <iomanip>
 
-int main(int argc, char* argv[]) {
+#define PAGE_SIZE = 256; // 8 bits para offset
+#define PAGE_TABLE_SIZE = 256; // 8 bits para page number
 
+int main() {
+  std::vector<int> address_list = {1, 256, 32768, 32769, 128, 65534, 33153};
+
+  std::cout << "Address"  << '\t'
+            << "Page Num" << '\t'
+            << "Offset"   << '\t'
+            << std::endl;
+
+  for (int address : address_list) {
+    int masked_address = address & 0xFFFF; // 16 bits
+    int page_number = (masked_address >> 8) & 0xFF; // bits 8-15
+    int offset = masked_address & 0xFF; // bits 0-7
+
+    std::cout << address << '\t'
+              << page_number     << '\t'
+              << offset          << '\t'
+              << std::endl;
+  }
   return 0;
 }
