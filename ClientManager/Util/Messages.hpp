c01ada93ee6ddf5d0d637a5d9df480f031b69926
Juan Pablo Vindas <juan@jpdebian11.com>
2025-10-28 10:33:33 -0600

    Messages.hpp fix

--- ClientManager/Util/Messages.hpp
+++ ClientManager/Util/Messages.hpp
@@ -26,7 +26,7 @@ enum class MessageType : uint8_t {
     SEN_FILE_BLOCKNUM_RES,
     SEN_FILE_BLOCK_REQ,
     SEN_FILE_BLOCK_RESP,
-    SEN_ADD_LOG
+    SEN_ADD_LOG,
     AUTH_LOGIN_REQ,
     AUTH_LOGIN_SUCCESS,
     ERR_COMMOM_MSG,
@@ -49,192 +49,202 @@ struct sensorFileName {
 };
 
 struct sensorNamesVector {
-  std::vector<sensorFileName> names;
+    std::vector<sensorFileName> names;
 };
 
 struct GenNumReq {
- uint32_t id_token;
+    uint32_t id_token;
 };
 
 struct fileNumberResp {
-  uint32_t totalFiles;
+    uint32_t totalFiles;
 };
 
 struct senFileNamesRes {
-  uint32_t page;
-  uint32_t totalPages;
-  sensorNamesVector fileNames;
+    uint32_t page;
+    uint32_t totalPages;
+    sensorNamesVector fileNames;
 };
 
 struct genSenFileReq {
-  uint32_t id_token;
-  sensorFileName fileName;
+    uint32_t id_token;
+    sensorFileName fileName;
 };
 
 struct senFileMetDRes {
-  sensorFileName fileName;
-  uint32_t size;
-  uint16_t permissions;
-  uint32_t userId;
-  uint32_t groupId;
-  uint32_t creationTime;
-  uint32_t lastModifiedTime;
-  uint32_t lastAccessTime; 
+    sensorFileName fileName;
+    uint32_t size;
+    uint16_t permissions;
+    uint32_t userId;
+    uint32_t groupId;
+    uint32_t creationTime;
+    uint32_t lastModifiedTime;
+    uint32_t lastAccessTime;
 };
 
 struct senFileBlockNumRes {
-  sensorFileName fileName;
-  uint32_t blocks;
+    sensorFileName fileName;
+    uint32_t blocks;
 };
 
-
 struct senFileBlockRes {
-  uint32_t page;
-  uint32_t totalPages;
-  uint8_t usedBlocks;
-  sensorFileName fileName;
-  std::string firstBlock;
-  std::string secondBlock;
+    uint32_t page;
+    uint32_t totalPages;
+    uint8_t usedBlocks;
+    sensorFileName fileName;
+    std::string firstBlock;
+    std::string secondBlock;
 };
 
 struct senAddLog {
-  sensorFileName fileName;
-  std::string data;
+    sensorFileName fileName;
+    std::string data;
+};
+
 struct authLoginReq {
-  std::string user;
-  std::string pass;
+    std::string user;
+    std::string pass;
 };
 
 struct authLoginSuccess {
-  token Token;
+    token Token;
 };
 
 struct errorCommonMsg {
-  std::string message;
+    std::string message;
 };
 
 struct genMessage {
-  uint8_t MID;
-  std::variant< GenNumReq, fileNumberResp, senFileNamesRes,
-                genSenFileReq, senFileMetDRes, senFileBlockNumRes,
-                senAddLog, senFileBlockRes, authLoginReq, authLoginSuccess, 
-                errorCommonMsg
-              > content;
+    uint8_t MID;
+    std::variant<
+        GenNumReq,
+        fileNumberResp,
+        senFileNamesRes,
+        genSenFileReq,
+        senFileMetDRes,
+        senFileBlockNumRes,
+        senAddLog,
+        senFileBlockRes,
+        authLoginReq,
+        authLoginSuccess,
+        errorCommonMsg
+    > content;
 };
 
 namespace bitsery {
 
-  template <typename S>
-  void serialize(S& s, token& t) {
-      s.value4b(t.id);
-      s.text1b(t.name, 20);
-      s.value1b(t.userType);
-      s.value1b(t.hour);
-      s.value1b(t.minute);
-  }
+    template <typename S>
+    void serialize(S& s, token& t) {
+        s.value4b(t.id);
+        s.text1b(t.name, 20);
+        s.value1b(t.userType);
+        s.value1b(t.hour);
+        s.value1b(t.minute);
+    }
 
-  template <typename S>
-  void serialize(S& s, sensorFileName& sf) {
-      s.text1b(sf.sensorType, 3);
-      s.value2b(sf.id);
-      s.value2b(sf.year);
-      s.value1b(sf.month);
-      s.value1b(sf.day);
-  }
+    template <typename S>
+    void serialize(S& s, sensorFileName& sf) {
+        s.text1b(sf.sensorType, 3);
+        s.value2b(sf.id);
+        s.value2b(sf.year);
+        s.value1b(sf.month);
+        s.value1b(sf.day);
+    }
 
-  template <typename S>
-  void serialize(S& s, sensorNamesVector& snv) {
-      // 1023 bytes. 93 sensorFile names posible
-      // (each filename has a 11 bytes size)
-      s.container(snv.names, 93);
-  }
+    template <typename S>
+    void serialize(S& s, sensorNamesVector& snv) {
+        s.container(snv.names, 93);
+    }
 
-  template <typename S>
-  void serialize(S& s, GenNumReq& m) {
-      s.value4b(m.id_token);
-  }
+    template <typename S>
+    void serialize(S& s, GenNumReq& m) {
+        s.value4b(m.id_token);
+    }
 
-  template <typename S>
-  void serialize(S& s, fileNumberResp& m) {
-      s.value4b(m.totalFiles);
-  }
+    template <typename S>
+    void serialize(S& s, fileNumberResp& m) {
+        s.value4b(m.totalFiles);
+    }
 
-  template <typename S>
-  void serialize(S& s, senFileNamesRes& m) {
-      s.value4b(m.page);
-      s.value4b(m.totalPages);
-      s.object(m.fileNames);
-  }
+    template <typename S>
+    void serialize(S& s, senFileNamesRes& m) {
+        s.value4b(m.page);
+        s.value4b(m.totalPages);
+        s.object(m.fileNames);
+    }
 
-  template <typename S>
-  void serialize(S& s, genSenFileReq& m) {
-      s.value4b(m.id_token);
-      s.object(m.fileName);
-  }
+    template <typename S>
+    void serialize(S& s, genSenFileReq& m) {
+        s.value4b(m.id_token);
+        s.object(m.fileName);
+    }
 
-  template <typename S>
-  void serialize(S& s, senFileMetDRes& m) {
-      s.object(m.fileName);
-      s.value4b(m.size);
-      s.value2b(m.permissions);
-      s.value4b(m.userId);
-      s.value4b(m.groupId);
-      s.value4b(m.creationTime);
-      s.value4b(m.lastModifiedTime);
-      s.value4b(m.lastAccessTime);
-  }
+    template <typename S>
+    void serialize(S& s, senFileMetDRes& m) {
+        s.object(m.fileName);
+        s.value4b(m.size);
+        s.value2b(m.permissions);
+        s.value4b(m.userId);
+        s.value4b(m.groupId);
+        s.value4b(m.creationTime);
+        s.value4b(m.lastModifiedTime);
+        s.value4b(m.lastAccessTime);
+    }
 
-  template <typename S>
-  void serialize(S& s, senFileBlockNumRes& m) {
-      s.object(m.fileName);
-      s.value4b(m.blocks);
-  }
+    template <typename S>
+    void serialize(S& s, senFileBlockNumRes& m) {
+        s.object(m.fileName);
+        s.value4b(m.blocks);
+    }
 
-  template <typename S>
-  void serialize(S& s, senFileBlockRes& m) {
-      s.value4b(m.page);
-      s.value4b(m.totalPages);
-      s.value1b(m.usedBlocks);
-      s.object(m.fileName);
-      s.text1b(m.firstBlock, 1024);
-      s.text1b(m.secondBlock, 1024);
-  }
+    template <typename S>
+    void serialize(S& s, senFileBlockRes& m) {
+        s.value4b(m.page);
+        s.value4b(m.totalPages);
+        s.value1b(m.usedBlocks);
+        s.object(m.fileName);
+        s.text1b(m.firstBlock, 1024);
+        s.text1b(m.secondBlock, 1024);
+    }
 
-  template <typename S>
-  void serialize(S& s, senAddLog& m) {
-      s.object(m.fileName);
-      s.text1b(m.data, 256);
-  void serialize(S& s, authLoginReq& m) {
-      s.text1b(m.user, 28);
-      s.text1b(m.pass, 28);
-  }
+    template <typename S>
+    void serialize(S& s, senAddLog& m) {
+        s.object(m.fileName);
+        s.text1b(m.data, 256);
+    }
 
-  template <typename S>
-  void serialize(S& s, authLoginSuccess& m) {
-      s.object(m.Token);
-  }
+    template <typename S>
+    void serialize(S& s, authLoginReq& m) {
+        s.text1b(m.user, 28);
+        s.text1b(m.pass, 28);
+    }
 
-  template <typename S>
-  void serialize(S& s, errorCommonMsg& m) {
-      s.text1b(m.message, MAX_ERR_MSG_LENGTH);
-  }
+    template <typename S>
+    void serialize(S& s, authLoginSuccess& m) {
+        s.object(m.Token);
+    }
 
-  template <typename S>
-  void serialize(S& s, genMessage& m) {
-      auto& mid = m.MID;   // referencia explícita
-      s.value1b(mid);
-      s.ext(m.content, bitsery::ext::StdVariant{});
-  }
+    template <typename S>
+    void serialize(S& s, errorCommonMsg& m) {
+        s.text1b(m.message, MAX_ERR_MSG_LENGTH);
+    }
+
+    template <typename S>
+    void serialize(S& s, genMessage& m) {
+        auto& mid = m.MID;
+        s.value1b(mid);
+        s.ext(m.content, bitsery::ext::StdVariant{});
+    }
 
 }
 
 template <typename T>
-T getMessageContent (const genMessage& message) {
-  try {
-    return std::get<T>(message.content);
-  } catch (const std::bad_variant_access&) {
-    throw std::runtime_error("ERROR: Could not extract message");
-  }
+T getMessageContent(const genMessage& message) {
+    try {
+        return std::get<T>(message.content);
+    } catch (const std::bad_variant_access&) {
+        throw std::runtime_error("ERROR: Could not extract message");
+    }
 }
 
 #endif // MESSAGES_HPP
