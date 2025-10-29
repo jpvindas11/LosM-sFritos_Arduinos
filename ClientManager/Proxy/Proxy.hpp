cc01d6575989ce9993d75f938cb0176da718a87b
Axel Rojas <axel.rojas.retana.dev@gmail.com>
2025-10-29 11:21:07 -0600

    agrego proxy concurrente

--- ClientManager/Proxy/Proxy.hpp
+++ ClientManager/Proxy/Proxy.hpp
@@ -4,28 +4,42 @@
 #include <string>
 #include <iostream>
 #include <algorithm>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <atomic>
 #include "../Util/Socket.hpp"
 #include "../Util/Messages.hpp"
+#include "../Util/Queue.hpp"
+
+struct QueuedMessage {
+  int arduinoSocket;
+  genMessage message;
+};
 
 class Proxy {
  private:
   std::string storageServerIP;
   int storageServerPort;
-  Socket storageSocket;
-  int storageSocketFD;
   
   std::string proxyIP;
   int proxyPort;
   Socket listeningSocket;
-  bool running;
+  std::atomic<bool> running;
+  
+  Queue<QueuedMessage> messageQueue;
+  
+  std::thread storageHandlerThread;
+  
+  std::vector<std::thread> clientThreads;
+  std::mutex threadsMutex;
   
   int startProxy(std::string proxyIP, int proxyPort, std::string storageIP,
     int storagePort);
   int listenForConnections(std::string ip, int port);
-  int acceptConnectionRequest();
   void acceptAllConnections();
-  bool connectToStorageServer();
-  void forwardSensorData(int arduinoSocket, genMessage& sensorData);
+  void forwardSensorData(genMessage& sensorData);
+  void processMessageQueue();
     
  public:
   Proxy();
